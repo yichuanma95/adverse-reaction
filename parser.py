dae432eb199c0d6c0923f6e30348916b7e4c2674
Yichuan Ma <yichuanma@yichuans-mbp.lan>
2022-08-30 21:38:15 -0400

    drafted parser

---
+++ parser.py
@@ -0,0 +1,61 @@
+import os
+
+from biothings.utils.common import open_anyfile
+
+def load_data(data_folder):
+    zipfile = os.path.join(data_folder, 'extract_extrait.zip')
+    assert os.path.exists(zipfile)
+
+    drugfile = os.path.join('cvponline_extract_20220430', 'report_drug.txt')
+    drugcols = \
+        ['report_drug_id', 'report_id', 'drug_product_id', 'drug_name', 'drug_role_en', 'drug_role_fr',
+         'route_admin_en', 'route_admin_fr', 'dose_qty', 'dose_unit_en', 'dose_unit_fr', 'frequency', 'freq_time',
+         'freq_time_en', 'freq_time_fr', 'freq_unit_en', 'freq_unit_fr', 'therapy_dur', 'therapy_dur_unit_en',
+         'therapy_dur_unit_fr', 'dosage_form_en', 'dosage_form_fr']
+    drug_reports = {}
+    with open_anyfile((zipfile, drugfile)) as f:
+        for line in f:
+            datapoint = line.rstrip('\n').split('$')
+            for i in range(len(datapoint)):
+                datapoint[i] = datapoint[i].strip('\"')
+            obj = {col: d for col, d in zip(drugcols, datapoint)}
+            del obj['report_drug_id']
+            id = obj.pop('report_id')
+            drug_reports.setdefault(id, []).append(obj)
+
+    reactionfile = os.path.join('cvponline_extract_20220430', 'reactions.txt')
+    reactioncols = \
+        ['reaction_id', 'report_id', 'duration', 'duration_unit_en', 'duration_unit_fr', 'reaction_term_en',
+         'reaction_term_fr', 'soc_en', 'soc_fr', 'meddra_ver']
+    reactions = {}
+    with open_anyfile((zipfile, reactionfile)) as f:
+        for line in f:
+            datapoint = line.rstrip('\n').split('$')
+            for i in range(len(datapoint)):
+                datapoint[i] = datapoint[i].strip('\"')
+            obj = {col: d for col, d in zip(reactioncols, datapoint)}
+            del obj['reaction_id']
+            id = obj.pop('report_id')
+            reactions.setdefault(id, []).append(obj)
+
+    reportfile = os.path.join('cvponline_extract_20220430', 'reports.txt')
+    reportcols = \
+        ['report_id', '_id', 'ver_no', 'latest_date_received', 'initial_date_received', 'mah_no', 'report_type_code',
+         'report_type_en', 'report_type_fr', 'gender_code', 'gender_en', 'gender_fr', 'age', 'age_yrs', 'age_unit_en',
+         'age_unit_fr', 'outcome_code', 'outcome_en', 'outcome_fr', 'weight', 'weight_unit_en', 'weight_unit_fr',
+         'height', 'height_unit_en', 'height_unit_fr', 'seriousness_code', 'seriousness_en', 'seriousness_fr',
+         'death', 'disability', 'congenital_anomaly', 'life_threatening', 'hosp_required', 'other_medically_imp_cond',
+         'reporter_type_en', 'reporter_type_fr', 'source_code', 'source_en', 'source_fr', 'e2b_safetyreport_id',
+         'authority_num', 'company_num']
+    with open_anyfile((zipfile, reportfile)) as f:
+        for line in f:
+            datapoint = line.rstrip('\n').split('$')
+            for i in range(len(datapoint)):
+                datapoint[i] = datapoint[i].strip('\"')
+            obj = {col: d for col, d in zip(reportcols, datapoint)}
+            id = obj.pop('report_id')
+            if id in drug_reports:
+                obj['drugs'] = drug_reports[id]
+            if id in reactions:
+                obj['reactions'] = reactions[id]
+            yield obj
